05788e0b028e198c5dbff43ed455bff355eab710
Raúl Jiménez <jimenezjrs@gmail.com>
2015-11-05 15:03:33 -0600

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comoVamosColombiaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the comoVamosColombiaApp
+ */
+angular.module('comoVamosColombiaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
