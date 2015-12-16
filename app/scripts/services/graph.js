efeb4baa318987132a4191b04d47b0e0822014ca
Ricardo Alanis <ricardo.alanis@gmail.com>
2015-12-16 14:15:30 -0600

    added function to color pallete to differentiate graphs

--- app/scripts/services/graph.js
+++ app/scripts/services/graph.js
@@ -19,9 +19,9 @@ angular.module('comoVamosColombiaApp')
       var _buildColorPallete = function(DataIndex) {
         switch(DataIndex) {
           case 0:
-            return ["FF7863","FF8558","FF924D","FF9F42","FFAC37","FFB92C","FFC621","FFD316","FFE00B","FFED00"]
-          case 1:
-            return ["E3000F","E61227","E9243F","EC3758","EF4970","F25C88","F56EA1","F881B9","FB93D1","FFA6EA"]
+            return ["#FF7863","#FF8558","#FF924D","#FF9F42","#FFAC37","#FFB92C","#FFC621","#FFD316","#FFE00B","#FFED00"]
+        case 1:
+            return ["#E3000F","#E61227","#E9243F","#EC3758","#EF4970","#F25C88","#F56EA1","#F881B9","#FB93D1","#FFA6EA"]
           }
       }
 
